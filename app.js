b097d975f0fe9fe907bff93ee07f906eacb26eda
Abolfazl Sadeghian <sadeghian.abolfazl@outlook.com>
2021-12-28 02:48:53 +0330

    api call

--- app.js
+++ app.js
@@ -7,7 +7,9 @@
 // Required libraries = prompt-sync
 
 const prompt = require('prompt-sync')({sigint: true});
-const request = require('request');
+// const request = require('request');
+const axios = require('axios');
+const { lstat } = require('fs');
 
 let astrology_signs = { aries :     { icon :'♈', description : '(Ram): March 21–April 19' },
                         taurus :    { icon :'♉', description : '(Bull): April 20–May 20' },
@@ -44,7 +46,8 @@ const welcome_and_choices = ()=>{
     }
     
     const user_choice = get_user_choice_and_validate()
-    present_results (make_api_call(user_choice))
+    make_api_call(user_choice)
+//   present_results(result)
 
 
 }
@@ -65,29 +68,38 @@ const get_user_choice_and_validate = () => {
 
 }
 
-const make_api_call =(user_choice) => {
+const make_api_call = (user_choice) => {
 
 let sign_to_check = sign_array[user_choice - 1]
-let recieved_data = ''
-    
-let options = {
-url: `https://aztro.sameerkumar.website/?sign=${sign_to_check}&day=today`,
-method: 'POST'
-};
+let url_line = `https://aztro.sameerkumar.website/?sign=${sign_to_check}&day=today`
 
-    function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
-        console.log(body)
-        recieved_data = JSON.parse(body)
-        }
-    }
+axios.post(url_line).then((result)=>{
+    console.log(result.data.description)
+})
 
-request(options, callback);
-return recieved_data
+
+
+// let options = {
+// url: `https://aztro.sameerkumar.website/?sign=${sign_to_check}&day=today`,
+// method: 'POST'
+// };
+
+//     function callback(error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//         recieved_data = body
+//         return recieved_data
+//         }
+//     }
+
+// console.log(request(options, callback));
+// console.log(recieved_data)
+// console.log(recieved_data)
+// return dataPromise
 }
 
 const present_results = (json) => {
-    
+    //console.log("((((",json)
+   // console.log(json.data,"lllllll")
 }
 
 welcome_and_choices()
